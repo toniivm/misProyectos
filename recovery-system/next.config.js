ac9964960664c922904fb0df47a5f23a6cbd47e7
toniiv7 <toniuve7@gmail.com>
2026-05-28 16:53:31 +0200

    chore: revert i18n property in next.config.js to support static export

--- recovery-system/next.config.js
+++ recovery-system/next.config.js
@@ -5,10 +5,6 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  i18n: {
-    locales: ['es', 'en'],
-    defaultLocale: 'es',
-  },
   trailingSlash: true,
   images: {
     unoptimized: true,
